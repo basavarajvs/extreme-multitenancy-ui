4d05db95406f303c4f7379ffb29bc11ffc02aacd
Raju <raju@example.com>
2025-09-03 11:35:25 +0530

    feat: replace default logo with Extreme Software logo

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -15,9 +15,9 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'Extreme SaaS',
   pwa: true,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: '/assets/images/logos/extreme-saas.png',
   iconfontUrl: '',
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
